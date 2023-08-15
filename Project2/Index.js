40aebb5954c81c4c50462e9c8b49d6245bb6b41a
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2023-08-16 07:52:21 +1000

    Just my old landing pages

---
+++ Project2/Index.js
@@ -0,0 +1,8 @@
+function imgSlider(Picture){
+    document.querySelector('.starbucks').src = Picture;
+  }
+
+  function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+  }
